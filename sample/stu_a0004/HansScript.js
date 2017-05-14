26655a41e321e41145ed6ae875ab9dfd43a58f0f
Rishi Agrawal <ragrawal@riverbed.com>
2017-05-14 13:11:15 +0530

    Added sample html files

---
+++ sample/stu_a0004/HansScript.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+        $('.HansDataTable').DataTable({
+//                "lengthMenu": [100, 50, 25, 10]
+                  "iDisplayLength": 100
+                
+        });
+});
